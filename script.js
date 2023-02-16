de47ee4558ef7c4761c472f35959d6d2aad1d081
cesarjakub <list>
2023-02-16 09:10:24 +0100

    feat: print Data

--- script.js
+++ script.js
@@ -7,6 +7,47 @@ class Zakaznik{
         this.Population = Population;
         this.Slug_Nation = Slug_Nation;
     }
+
+    vypis(){
+        let zprava = `<div class="card m-2" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID nation: ${this.ID_Nation}</li>
+          <li class="list-group-item">Nation: ${this.Nation}</li>
+          <li class="list-group-item">ID year: ${this.ID_Year}</li>
+          <li class="list-group-item">Year: ${this.Year}</li>
+          <li class="list-group-item">Population: ${this.Population}</li>
+          <li class="list-group-item">Slug nation: ${this.Slug_Nation}</li>
+        </ul>
+      </div>`;
+      return zprava
+    }
+
+    vypisTable(){
+        let zprava = `<table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID Nation</th>
+            <th scope="col">Nation</th>
+            <th scope="col">ID year</th>
+            <th scope="col">Year</th>
+            <th scope="col">Population</th>
+            <th scope="col">Slug nation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>${this.ID_Nation}</td>
+            <td>${this.Nation}</td>
+            <td>${this.ID_Year}</td>
+            <td>${this.Year}</td>
+            <td>${this.Population}</td>
+            <td>${this.Slug_Nation}</td>
+          </tr>
+        </tbody>
+      </table>`;
+      return zprava;
+    }
 }
 
 class EvidenceZakazniku{
@@ -42,5 +83,35 @@ class EvidenceZakazniku{
     delZak(id){
         this.zakaznici.remove(id);
     }
+
+    print(){
+        let dataCard = "";
+        this.zakaznici.forEach(zak =>{
+            dataCard += zak.vypis();
+        });
+        let main = document.getElementById("main");
+        main.innerHTML = dataCard;
+    }
+
+    printTable(){
+        let dataTab = "";
+        this.zakaznici.forEach(zak =>{
+            dataTab += zak.vypisTable();
+        });
+        let main = document.getElementById("main");
+        main.innerHTML = dataTab;
+    }
+
 }
-new EvidenceZakazniku();
+let evidence = new EvidenceZakazniku();
+
+let submitKrty = document.getElementById("karty");
+let tabulkaData = document.getElementById("dataTable");
+
+submitKrty.addEventListener("click", () => {
+    evidence.print();
+});
+
+tabulkaData.addEventListener("click", ()=>{
+    evidence.printTable();
+});
